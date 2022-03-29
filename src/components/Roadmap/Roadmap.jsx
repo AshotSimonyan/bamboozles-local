d7f3b8427c4c7fb10457245e84f38ddbc6a649e8
AshotUltraLabs <a.simonyan.stdev@gmail.com>
2022-03-30 00:38:43 +0400

    Update Roadmap.jsx

--- src/components/Roadmap/Roadmap.jsx
+++ src/components/Roadmap/Roadmap.jsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useState } from "react"
+import React, { forwardRef, useEffect, useState } from "react"
 import { theme } from "../../styles/global/theme"
 import { RoadmapStyle } from "./Roadmap.style"
 import Timeline from "../TimeLine/TimeLine"
@@ -7,9 +7,28 @@ import { Title } from "../Animations"
 import { Fade } from "react-awesome-reveal"
 
 const Roadmap = forwardRef((props, ref) => {
+  const [percent, setPercent] = useState(0)
   const onCallback = e => {
     console.log(e)
   }
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      const roadmapHeight = ref?.current.clientHeight
+      const pageY = window.pageYOffset
+      // console.log(roadmapHeight)
+      if(pageY >= roadmapHeight) {
+        const timeLineHeight = Math.round((pageY - roadmapHeight) / roadmapHeight * 100)
+        if(timeLineHeight >= 100) {
+          return setPercent(100)
+        }
+        setPercent(timeLineHeight)
+      }
+    })
+  }, [])
+
+  console.log(percent)
+
   return (
     <RoadmapStyle ref={ref}>
       <div className="container">
